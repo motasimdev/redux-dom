59c654d08799d88ed7e8c81f8841a4992e69d18f
MD Motasim Billah <themotasimdev@gmail.com>
2026-07-07 01:26:48 +0600

    something

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -38,12 +38,35 @@ const CartContext = createContext();
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
-  const addToCart = (product)=>{
-    setCartItems((prevItems)=>[...prevItems, product])
-  }
-  console.log(cartItems)
+  const addToCart = (product) => {
+    const existingProduct = cartItems.find((item) => item.id === product.id);
+    console.log(cartItems);
+    if (!existingProduct) {
+      setCartItems((prevItems) => [
+        ...prevItems,
+        {
+          ...product,
+          quantity: 1,
+        },
+      ]);
+    } else {
+      setCartItems((prevItems) =>
+        prevItems.map((item) => {
+          if (item.id === product.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+          return item;
+        }),
+      );
+    }
+  };
   return (
-    <CartContext.Provider value={{ cartItems, addToCart }}>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
   );
 };
 export default CartContext;
